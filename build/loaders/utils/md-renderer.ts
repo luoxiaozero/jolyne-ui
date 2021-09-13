b570bf0c615bdc98304767f7ed4cb26c527e1129
罗小 <liziqiang01@163.com>
2021-09-13 19:44:26 +0800

    ✨ feat(build): 解析 markdown 中的表格

--- build/loaders/utils/md-renderer.ts
+++ build/loaders/utils/md-renderer.ts
@@ -3,5 +3,16 @@ import hljs from "highlight.js";
 
 export default function createMdRenderer() {
   const renderer = new marked.Renderer();
+  const overrides = {
+    table(header: string, body: string) {
+      return `<jo-table>
+      <thead> ${header}</thead>
+      <tbody>${body}</tbody>
+      </jo-table>`;
+    },
+  };
+  Object.keys(overrides).forEach((key) => {
+    renderer[key] = overrides[key];
+  });
   return renderer;
 }
