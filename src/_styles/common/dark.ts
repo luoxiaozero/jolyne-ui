74db7cb814ac4793eef77317730cf48a469c7ab1
罗小 <liziqiang01@163.com>
2021-09-13 10:45:59 +0800

    🐞 fix: button 组件颜色

--- src/_styles/common/dark.ts
+++ src/_styles/common/dark.ts
@@ -3,10 +3,25 @@ const theme = {
   ...common,
   fontColor: "#ffffffe6",
   colorBase: "#000",
-  colorInvertBase: "#fff",
+  colorBaseInvert: "#fff",
   colorBody: "rgb(16, 16, 20)",
   neutralPopover: "rgb(72, 72, 78)",
   neutralCard: "#21262d",
   neutralModal: "rgb(44, 44, 50)",
+
+  colorPrimary: "#12aa9c",
+  colorPrimaryHover: "#10a5a5",
+
+  colorInfo: "#00a7de",
+  colorInfoHover: "#4af",
+
+  colorSuccess: "#18a058",
+  colorSuccessHover: "#36ad6a",
+
+  colorWarning: "#f0a020",
+  colorWarningHover: "#fcb040",
+
+  colorError: "#d03050",
+  colorErrorHover: "#de576d",
 };
 export default theme;
--- src/_styles/common/light.ts
+++ src/_styles/common/light.ts
@@ -3,11 +3,26 @@ const theme = {
   ...common,
   fontColor: "#333639",
   colorBase: "#fff",
-  colorInvertBase: "#000",
+  colorBaseInvert: "#000",
   colorBody: "#f6f6f6",
   neutralPopover: "#fff",
   neutralCard: "#fff",
   neutralModal: "#fff",
   neutralBody: "#f6f6f6",
+
+  colorPrimary: "#12aa9c",
+  colorPrimaryHover: "#10a5a5",
+
+  colorInfo: "#00a7de",
+  colorInfoHover: "#4af",
+
+  colorSuccess: "#18a058",
+  colorSuccessHover: "#36ad6a",
+
+  colorWarning: "#f0a020",
+  colorWarningHover: "#fcb040",
+
+  colorError: "#d03050",
+  colorErrorHover: "#de576d",
 };
 export default theme;
