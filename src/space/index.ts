18e1e9c5350b601e7bab8d8c6deb2d64b7e794b2
罗小 <liziqiang01@163.com>
2021-09-17 21:45:42 +0800

    ✨ feat: space 组件自定义间距

--- src/space/index.ts
+++ src/space/index.ts
@@ -1,1 +1,1 @@
-export { default as JoSpace } from "./Space";
+export { default as JoSpace } from "./src/Space";
--- src/space/src/Space.tsx
+++ src/space/src/Space.tsx
@@ -1,5 +1,5 @@
-import { flatten } from "../util/flatten";
-import getSlot from "../util/getSlot";
+import { flatten } from "../../util/flatten";
+import getSlot from "../../util/getSlot";
 import { h, defineComponent, PropType } from "vue";
 import "./styles/index.css";
 
@@ -15,8 +15,26 @@ export default defineComponent({
       type: Boolean,
       default: true,
     },
+    size: {
+      type: [Number, Array] as PropType<number | [number, number]>,
+      default: 8,
+    }
   },
-  setup() {},
+  setup(props) {
+    let horizontalMargin = "";
+    let verticalMargin = "";
+    if (typeof props.size === "number") {
+      horizontalMargin = props.size + "px";
+      verticalMargin = props.size + "px";
+    } else {
+      horizontalMargin = props.size[0] + "px";
+      verticalMargin = props.size[1] + "px";
+    }
+    return {
+      horizontalMargin,
+      verticalMargin,
+    }
+  },
   render() {
     const children = flatten(getSlot(this));
     const lastIndex = children.length - 1;
@@ -35,10 +53,10 @@ export default defineComponent({
               style={
                 (this.$props.vertical
                   ? {
-                      marginBottom: index !== lastIndex ? "8px" : "",
+                      marginBottom: index !== lastIndex ? this.verticalMargin : "",
                     }
                   : {
-                      marginRight: index !== lastIndex ? "8px" : "",
+                      marginRight: index !== lastIndex ? this.horizontalMargin : "",
                       paddingTop: "4px",
                       paddingBottom: "4px",
                     }) as any
