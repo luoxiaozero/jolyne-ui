f2d115b9c59512390577dd2c2d9f6ff825fe4339
罗小 <liziqiang01@163.com>
2021-09-18 08:55:18 +0800

    🐞 fix: scrollbar 组件默认样式

--- src/scrollbar/src/ScrollBar.tsx
+++ src/scrollbar/src/ScrollBar.tsx
@@ -3,11 +3,16 @@ import "./styles/index.css";
 export default defineComponent({
   name: "ScrollBar",
   props: {
+    /**滚动条默认样式功能 */
+    scrollStyle: {
+      type: String as PropType<"default" | "hidden" | "remove">,
+      default: "hidden"
+    },
     contentStyle: [String, Object] as PropType<CSSProperties | string>,
   },
   render() {
     return (
-      <div class="jo-scrollbar" style={this.contentStyle}>
+      <div class={`jo-scrollbar jo-scrollbar--${this.scrollStyle}`} style={this.contentStyle}>
         {this.$slots.default?.()}
       </div>
     );
