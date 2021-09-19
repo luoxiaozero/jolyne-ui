17d896cddb8c1d54fde2abde0fa477351b57809f
罗小 <liziqiang01@163.com>
2021-09-19 17:51:17 +0800

    ✨ feat(build): 代码高亮

--- build/loaders/utils/md-renderer.ts
+++ build/loaders/utils/md-renderer.ts
@@ -10,6 +10,11 @@ export default function createMdRenderer() {
       <tbody>${body}</tbody>
       </jo-table></jo-card>`;
     },
+    code(code: string, language: string) {
+      console.log(language);
+      const highlighted = hljs.highlight(code, {language}).value;
+      return `<jo-card><jo-code><pre>${highlighted}</pre></jo-code></jo-card>`
+    }
   };
   Object.keys(overrides).forEach((key) => {
     renderer[key] = overrides[key];
