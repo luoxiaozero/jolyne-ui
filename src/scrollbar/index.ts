02c673e6c655b79f17fe9b53c5e8d4c8ffe9ad0c
luoxiao <luoxiaozero@163.com>
2022-10-24 13:42:50 +0800

    🐞 fix: Scrollbar文件名拼写错误

--- src/scrollbar/index.ts
+++ src/scrollbar/index.ts
@@ -1,3 +1,3 @@
-export { default as JoScrollbar } from "./src/ScrollBar";
+export { default as JoScrollbar } from "./src/Scrollbar";
 
 export type { ScrollbarProps as JTScrollbarProps } from "./src/Scrollbar"
